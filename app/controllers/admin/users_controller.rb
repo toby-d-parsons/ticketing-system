578ea7cf11b59b6492f051b9339f3164698fc24c
Toby Parsons <toby.d.parsons@gmail.com>
2024-12-27 20:39:50 +0000

    feat: Add page for creating new users with email, role, and password fields
    - Created a new page to allow the creation of users, including setting their email address, role, and password.
    - Incorporated form fields for selecting a role and entering user details.
    - Integrated functionality to assign roles during user creation.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,6 +3,27 @@ class Admin::UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+    @roles = Role.all
+  end
+
+  def create
+    @user = User.new(user_params)
+    @roles = Role.all
+    @role = @user.role.name
+    if @user.save
+      redirect_to admin_users_index_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def edit
+  end
+
+  private
+  def user_params
+    params.expect(user: [ :email_address, :role_id, :password, :password_confirmation ])
   end
 end
