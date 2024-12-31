cb182128eab97cafe15ca5805f2e19ff67d187da
Toby Parsons <toby.d.parsons@gmail.com>
2024-12-31 12:43:05 +0000

    feat: Add RSpec tests and validations for User email and password
    - Added RSpec tests for `email_address` validation:
      - Must be unique.
      - Must follow a valid email format.
    - Added RSpec tests for `password` validation:
      - Must be at least 8 characters.
      - Must include at least one lowercase letter, one uppercase letter, one number, and one symbol.
    - Updated `User` model to implement validations for email and password to pass these tests.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,19 @@ class User < ApplicationRecord
   has_many :tickets
   belongs_to :role
 
-  validates :email_address, presence: true
+  validates :email_address, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  PASSWORD_REQUIREMENTS = /\A
+    (?=.{8,}) # At least 8 characters long
+    (?=.*[a-z]) # Contains at least 1 lowercase letter
+    (?=.*[A-Z]) # Contains at least 1 uppercase letter
+    (?=.*\d) # Contains at least 1 number
+    (?=.*[[:^alnum:]]) # Contains at least 1 symbol
+  /x
+
+  validates :password, format: { with: PASSWORD_REQUIREMENTS,
+                                 message: "must be 8 or more characters, including lower and upper case letters and at least one number and symbol" },
+                                 if: :password_digest_changed?
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ describe User do
   subject do
     described_class.create(
       email_address: "testemail@email.com",
-      password: "password",
+      password: "passworD12£$",
       role_id: role.id
       )
   end
@@ -29,6 +29,21 @@ describe User do
         expect(subject).to be_invalid
       end
 
+      it "is invalid without a valid email_address (missing username)" do
+        subject.email_address = "@gmail.com"
+        expect(subject).to be_invalid
+      end
+
+      it "is invalid without a valid email_address (missing mail server and domain)" do
+        subject.email_address = "test@"
+        expect(subject).to be_invalid
+      end
+
+      it "is invalid without a valid email_address (missing symbol)" do
+        subject.email_address = "testgmail.com"
+        expect(subject).to be_invalid
+      end
+
       it "is invalid without a role_id" do
         subject.role_id = nil
         expect(subject).to be_invalid
@@ -36,6 +51,46 @@ describe User do
 
       it "is invalid without a valid role_id" do
         subject.role_id = 200 # Assuming that no role with id 200 exists
+        expect(subject).to be_invalid
+      end
+
+      it "is invalid when duplicate email_address is entered" do
+        described_class.create(
+          email_address: "duplicate@test.com",
+          password: "passworD12£$",
+          role_id: role.id
+        )
+        subject.email_address = "duplicate@test.com"
+        expect(subject).to be_invalid
+      end
+
+      it "is invalid without a valid password" do
+        subject.password = "test"
+        expect(subject).to be_invalid
+      end
+
+      it "is invalid without a valid password (< 8 characters)" do
+        subject.password = "Test1!"
+        expect(subject).to be_invalid
+      end
+
+      it "is invalid without a valid password (No lowercase letters)" do
+        subject.password = "TESTING!1"
+        expect(subject).to be_invalid
+      end
+
+      it "is invalid without a valid password (No uppercase letters)" do
+        subject.password = "testing!1"
+        expect(subject).to be_invalid
+      end
+
+      it "is invalid without a valid password (No symbols)" do
+        subject.password = "Testing1"
+        expect(subject).to be_invalid
+      end
+
+      it "is invalid without a valid password (No numbers)" do
+        subject.password = "Testiiing!"
         expect(subject).to be_invalid
       end
     end
