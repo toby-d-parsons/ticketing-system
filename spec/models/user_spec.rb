975551021f2948a4fa20caa184b40d896e15785c
Toby Parsons <toby.d.parsons@gmail.com>
2024-12-31 13:17:26 +0000

    chore: Reorganize `user_spec.rb` structure and add `type: :model` metadata
    - Refactored `user_spec.rb` to improve organization and readability.
    - Added `type: :model` metadata to support future RSpec configurations and tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe User do
+describe User, type: :model do
   let!(:role) { Role.create(name: 'Admin') }
 
   subject do
@@ -11,87 +11,88 @@ describe User do
       )
   end
 
-  describe "validations on creation" do
-    context "when valid attributes are provided" do
+  context "validations on creation" do
+    context "valid attributes" do
       it "is valid with an email_address, password, and valid role_id" do
         expect(subject).to be_valid
       end
     end
 
-    context "when invalid attributes are provided" do
-      it "is invalid without a password" do
-        subject.password = nil
-        expect(subject).to be_invalid
+    context "invalid attributes" do
+      context "email address" do
+        it "is invalid without an email_address" do
+          subject.email_address = nil
+          expect(subject).to be_invalid
+        end
+
+        it "is invalid without a username" do
+          subject.email_address = "@gmail.com"
+          expect(subject).to be_invalid
+        end
+
+        it "is invalid without a mail server and domain" do
+          subject.email_address = "test@"
+          expect(subject).to be_invalid
+        end
+
+        it "is invalid without an @ symbol" do
+          subject.email_address = "testgmail.com"
+          expect(subject).to be_invalid
+        end
+
+        it "is invalid when a duplicate email_address is entered" do
+          described_class.create(
+            email_address: "duplicate@test.com",
+            password: "passworD12£$",
+            role_id: role.id
+          )
+          subject.email_address = "duplicate@test.com"
+          expect(subject).to be_invalid
+        end
       end
 
-      it "is invalid without an email_address" do
-        subject.email_address = nil
-        expect(subject).to be_invalid
+      context "password" do
+        it "is invalid without a password" do
+          subject.password = nil
+          expect(subject).to be_invalid
+        end
+
+        it "is invalid with less than 8 characters" do
+          subject.password = "Test1!"
+          expect(subject).to be_invalid
+        end
+
+        it "is invalid without lowercase letters" do
+          subject.password = "TESTING!1"
+          expect(subject).to be_invalid
+        end
+
+        it "is invalid without uppercase letters" do
+          subject.password = "testing!1"
+          expect(subject).to be_invalid
+        end
+
+        it "is invalid without symbols" do
+          subject.password = "Testing1"
+          expect(subject).to be_invalid
+        end
+
+        it "is invalid without numbers" do
+          subject.password = "Testiiing!"
+          expect(subject).to be_invalid
+        end
       end
 
-      it "is invalid without a valid email_address (missing username)" do
-        subject.email_address = "@gmail.com"
-        expect(subject).to be_invalid
-      end
+      context "role_id" do
+        it "is invalid without a role_id" do
+          subject.role_id = nil
+          expect(subject).to be_invalid
+        end
 
-      it "is invalid without a valid email_address (missing mail server and domain)" do
-        subject.email_address = "test@"
-        expect(subject).to be_invalid
-      end
-
-      it "is invalid without a valid email_address (missing symbol)" do
-        subject.email_address = "testgmail.com"
-        expect(subject).to be_invalid
-      end
-
-      it "is invalid without a role_id" do
-        subject.role_id = nil
-        expect(subject).to be_invalid
-      end
-
-      it "is invalid without a valid role_id" do
-        subject.role_id = 200 # Assuming that no role with id 200 exists
-        expect(subject).to be_invalid
-      end
-
-      it "is invalid when duplicate email_address is entered" do
-        described_class.create(
-          email_address: "duplicate@test.com",
-          password: "passworD12£$",
-          role_id: role.id
-        )
-        subject.email_address = "duplicate@test.com"
-        expect(subject).to be_invalid
-      end
-
-      it "is invalid without a valid password" do
-        subject.password = "test"
-        expect(subject).to be_invalid
-      end
-
-      it "is invalid without a valid password (< 8 characters)" do
-        subject.password = "Test1!"
-        expect(subject).to be_invalid
-      end
-
-      it "is invalid without a valid password (No lowercase letters)" do
-        subject.password = "TESTING!1"
-        expect(subject).to be_invalid
-      end
-
-      it "is invalid without a valid password (No uppercase letters)" do
-        subject.password = "testing!1"
-        expect(subject).to be_invalid
-      end
-
-      it "is invalid without a valid password (No symbols)" do
-        subject.password = "Testing1"
-        expect(subject).to be_invalid
-      end
-
-      it "is invalid without a valid password (No numbers)" do
-        subject.password = "Testiiing!"
-        expect(subject).to be_invalid
+        it "is invalid with an invalid role_id" do
+          subject.role_id = 200 # Assuming that no role with id 200 exists
+          expect(subject).to be_invalid
+        end
       end
     end
   end
